2edce445a3adfa64ee5be4f1959a6a8ea8198da0
janzadnik <janzadnik@icloud.com>
2018-10-01 13:48:53 +0200

    Vytvoření základu pro todo listy

--- src/ui/ProFrame.java
+++ src/ui/ProFrame.java
@@ -1,12 +1,20 @@
 package ui;
 
+import model.TableModel;
+import model.ToDoItem;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.List;
 
 public class ProFrame extends JFrame {
 
     static int width = 800;
     static int height = 600;
+    private TableModel model;
 
 
 
@@ -32,11 +40,65 @@ public class ProFrame extends JFrame {
         button.setText("Přidat poznámku");
         toolbar.add(button);
 
-        JTable table = new JTable();
+        JButton buttonUloz = new JButton();
+        buttonUloz.setText("Ulož");
+        toolbar.add(buttonUloz);
+
+
+        JButton buttonNacti = new JButton();
+        buttonNacti.setText("Načti");
+        toolbar.add(buttonNacti);
+
+        button.addActionListener(e -> {
+            ToDoItem item = new ToDoItem("Test obsah");
+            model.add(item);
+                });
+
+
+        buttonUloz.addActionListener(e -> {
+            saveItems();
+        });
+
+
+        buttonNacti.addActionListener(e -> {
+            loadItems();
+        });
+
+
+        model = new TableModel();
+
+
+        JTable table = new JTable(model);
         add(new JScrollPane(table),BorderLayout.CENTER);
         pack();
 
+        setLocationRelativeTo(null); //center okna na obrazovce
 
+
+    }
+
+
+    private void loadItems() {
+        try {
+            ObjectInputStream stream = new ObjectInputStream(new FileInputStream(new File("our.db")));
+            List<ToDoItem> items = (List<ToDoItem>) stream.readObject();
+            stream.close();
+            model.setItems(items);
+            model.fireTableDataChanged();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveItems(){
+        try {
+            ObjectOutput stream = new ObjectOutputStream(new FileOutputStream(new File("our.db")));
+            stream.writeObject(model.getItems());
+            stream.flush();
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
